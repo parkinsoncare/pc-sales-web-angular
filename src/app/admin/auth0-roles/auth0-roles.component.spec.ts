8b01129128199b136a0cb420d12cfb7fcb57e6cd
Patrick Nee <pnetsmail-ms@yahoo.com>
2019-09-25 21:36:31 +0200

    Init commit

--- src/app/admin/auth0-roles/auth0-roles.component.spec.ts
+++ src/app/admin/auth0-roles/auth0-roles.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavBarComponent } from './nav-bar.component';
+import { Auth0RolesComponent } from './auth0-roles.component';
 
-describe('NavBarComponent', () => {
-  let component: NavBarComponent;
-  let fixture: ComponentFixture<NavBarComponent>;
+describe('Auth0RolesComponent', () => {
+  let component: Auth0RolesComponent;
+  let fixture: ComponentFixture<Auth0RolesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavBarComponent ]
+      declarations: [ Auth0RolesComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavBarComponent);
+    fixture = TestBed.createComponent(Auth0RolesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/admin/auth0-roles/auth0-roles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth0-roles',
+  templateUrl: './auth0-roles.component.html',
+  styleUrls: ['./auth0-roles.component.scss']
+})
+export class Auth0RolesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
