cb52ab727316f7c67cd3d0f0736ae912afad5a8f
Patrick N <pnetsmail-ms@yahoo.com>
2020-04-28 16:45:21 +0200

    Remove everything excess

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { PublicRootComponent } from './public/public-root/public-root.component';
-import { ContactUsComponent } from './public/contact-us/contact-us.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 const routes: Routes = [
@@ -10,8 +9,7 @@ const routes: Routes = [
   { path: 'public', component: PublicRootComponent,
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'contactus', component: ContactUsComponent }
+      { path: 'home', component: HomeComponent }
     ]
   }
 ];
